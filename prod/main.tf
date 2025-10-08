cbf992672587fb9939d3801e5743b9c18ca1a1ab
sys-cross <spreadsheeterror@gmail.com>
2025-10-08 13:59:27 +0100

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -5,14 +5,6 @@ terraform {
       version = "5.24.0"
     }
   }
-    backend "s3" {
-    bucket = "digger-s3backend-quickstart-aws"              # Change if a different S3 bucket name was used for the backend 
-    /* Un-comment to use DynamoDB state locking
-    dynamodb_table = "digger-locktable-quickstart-aws"      # Change if a different DynamoDB table name was used for backend
-    */
-    key    = "terraform/state"
-    region = "us-east-1"
-  }
 }
 
 provider "aws" {
